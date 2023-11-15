7df74d54823dd792b5fb276875eea0d738602a97
seungjun <149456880+el1win2@users.noreply.github.com>
2023-11-15 11:24:26 +0900

    2023.11.15
    두 수의 차, 두 수의 곱

---
+++ MinusTwoNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+public class MinusTwoNumbers
+{
+    public int solution(int num1, int num2)
+    {
+        if (-50000 > num1 || num1 > 50000) num1 = 0;
+        if (-50000 > num2 || num2 > 50000) num2 = 0;
+        int answer = num1 - num2;
+        return answer;
+    }
+}
---
+++ MultiplyingTwoNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+
+public class MultiplyingTwoNumbers
+{
+    public int solution(int num1, int num2)
+    {
+        if (0 > num1 || num1 > 100) num1 = 0;
+        if (0 > num2 || num2 > 100) num2 = 0;
+        int answer = num1 * num2;
+        return answer;
+    }
+}
