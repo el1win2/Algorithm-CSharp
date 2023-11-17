798e2b03d4de221278da5308a570cdc31872d7c3
seungjun <149456880+el1win2@users.noreply.github.com>
2023-11-17 09:45:32 +0900

    2023.11.17
    나이 출력

---
+++ AgeOutput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 나이 출력
+internal class AgeOutput
+{
+    public int solution(int age)
+    {
+        int year = 2022;
+        if (age < 0 || age > 120) age = 1;
+        for (int i = 1; i < age; i++)
+        {
+            year--;
+        }
+        int answer = year;
+        return answer;
+    }
+}
+
