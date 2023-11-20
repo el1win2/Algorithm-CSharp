5055eaa8ca90bf0364a62df85c90345852ad3b9a
seungjun <149456880+el1win2@users.noreply.github.com>
2023-11-20 09:20:07 +0900

    2023.11.20
    숫자 비교하기, 두 수의 합

---
+++ CompareTheNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 숫자 비교하기
+internal class CompareTheNumbers
+{
+    public int solution(int num1, int num2)
+    {
+        if (0 > num1 || num1 > 10000) num1 = 0;
+        if (0 > num2 || num2 > 10000) num2 = 0;
+        if (num1 == num2)
+        {
+            return 1;
+        }
+        else { return -1; }
+    }
+}
---
+++ PlusTwoNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 두 수의 합
+internal class PlusTwoNumbers
+{
+    public int solution(int num1, int num2)
+    {
+        if (-50000 > num1 || num1 > 50000) num1 = 0;
+        if (-50000 > num2 || num2 > 50000) num2 = 0;
+        int answer = num1 + num2;
+        return answer;
+    }
+}
