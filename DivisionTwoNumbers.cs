0ed6ab0225fe279d1c41f1104fdf7202c141056b
seungjun <149456880+el1win2@users.noreply.github.com>
2023-11-21 09:21:02 +0900

    2023.11.21
    두 수의 나눗셈, 각도기

---
+++ DivisionTwoNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 두 수의 나눗셈
+internal class DivisionTwoNumbers
+{
+    public int solution(int num1, int num2)
+    {
+        if (1 > num1 || num1 > 100) num1 = 1;
+        if (1 > num2 || num2 > 100) num2 = 1;
+        float answer = (float)num1 / (float)num2 * 1000;
+        return (int)answer;
+    }
+}
---
+++ Protractor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 각도기
+internal class Protractor
+{
+    public int solution(int angle)
+    {
+        int answer = 0;
+        if (1 > angle || angle > 180) angle = 1;
+        if (0 < angle && angle < 90)
+        {
+            answer = 1;
+        }
+        else if (angle == 90)
+        {
+            answer = 2;
+        }
+        else if (90 < angle && angle < 180)
+        {
+            answer = 3;
+        }
+        else
+        {
+            answer = 4;
+        }
+
+        return answer;
+    }
+}
