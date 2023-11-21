f59c14eb933b4a74918e102c0a670628492d2528
seungjun <149456880+el1win2@users.noreply.github.com>
2023-11-21 09:43:18 +0900

    2023.11.21 2
    짝수의 합

---
+++ SumEvenNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 짝수의 합
+internal class SumEvenNumbers
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        if (1 > n || n > 1000) n = 1;
+        for (int i = 0; i <= n; i++)
+        {
+            if (i % 2 == 0) answer += i;
+        }
+        return answer;
+    }
+}
