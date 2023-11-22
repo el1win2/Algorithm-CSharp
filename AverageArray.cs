c87ca1a3d0cfc6e31b9a970235473a5f05537301
seungjun <149456880+el1win2@users.noreply.github.com>
2023-11-22 10:03:21 +0900

    2023.11.22
    배열의 평균값

---
+++ AverageArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 배열의 평균값
+internal class AverageArray
+{
+    public double solution(int[] numbers)
+    {
+        double num1 = 0;
+        double num2 = 0;
+        foreach (int i in numbers)
+        {
+            num1++;
+            num2 += i;
+        }
+        double answer = num2 / num1;
+        return answer;
+    }
+}
