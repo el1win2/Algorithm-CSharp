305a7bfa1ddf9ec9116a5fabea05f465a9a61d9d
seungjun <149456880+el1win2@users.noreply.github.com>
2023-11-23 09:32:38 +0900

    2023.11.23
    짝수와 홀수

---
+++ EvenAndOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 짝수와 홀수
+internal class EvenAndOdd
+{
+    public string solution(int num)
+    {
+        string answer;
+        if (num % 2 == 0)
+        {
+            answer = "Even";
+        }
+        else
+        {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
