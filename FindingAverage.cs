5af4c443ea3fffc9a8552497fb6e1508878ded3e
seungjun <149456880+el1win2@users.noreply.github.com>
2023-11-27 09:57:42 +0900

    2023.11.27
    평균 구하기

---
+++ FindingAverage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 평균 구하기
+internal class FindingAverage
+{
+    public double solution(int[] arr)
+    {
+        int num1 = 0;
+        int num2 = 0;
+        foreach (int i in arr)
+        {
+            num1 += i;
+            num2++;
+        }
+        double answer = (double)num1 / (double)num2;
+        return answer;
+    }
+}
