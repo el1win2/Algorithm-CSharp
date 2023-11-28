5b568faafa5efeec517a1bf8b7901cd9ec22c01f
seungjun <149456880+el1win2@users.noreply.github.com>
2023-11-28 10:21:03 +0900

    2023.11.28
    자릿수 더하기

---
+++ AddDigits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 자릿수 더하기
+internal class AddDigits
+{
+    public int solution(int n)
+    {
+        int num = 0;
+        foreach (char s in n.ToString())
+        {
+            int i = int.Parse(s.ToString());
+            num += i;
+        }
+        int answer = num;
+        return answer;
+    }
+}
---
+++ test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 테스트용
+internal class test
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
