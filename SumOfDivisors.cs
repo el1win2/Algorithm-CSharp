6cbe10f836899c1b47923bd546e1132a05644dea
seungjun <149456880+el1win2@users.noreply.github.com>
2023-11-29 09:53:52 +0900

    2023.11.29
    약수의 합

---
+++ SumOfDivisors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 약수의 합
+internal class SumOfDivisors
+{
+    public int solution(int n)
+    {
+        int num = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                num += i;
+            }
+        }
+        int answer = num;
+        return answer;
+    }
+}
