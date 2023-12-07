ac1041684a2f33f46f8647f4aa3bc64d181f1181
seungjun <149456880+el1win2@users.noreply.github.com>
2023-12-07 09:57:01 +0900

    2023.12.07
    문자열을 정수로 바꾸기

---
+++ ReplaceAStringWithAnInteger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// 문자열을 정수로 바꾸기
+internal class ReplaceAStringWithAnInteger
+{
+    public int solution(string s)
+    {
+        int answer = int.Parse(s);
+        return answer;
+    }
+}
