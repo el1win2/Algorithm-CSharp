ee775d56fa89be8c1ae30b63da4b0726793d5231
seungjun <149456880+el1win2@users.noreply.github.com>
2023-12-11 10:46:23 +0900

    2023.12.11
    x만큼 간격이 있는 n개의 숫자

---
+++ NNumbersSpacedByX.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp;
+// x만큼 간격이 있는 n개의 숫자
+internal class NNumbersSpacedByX
+{
+    public long[] solution(int x, int n)
+    {
+        long[] answer = new long[n];
+        for (int i = 0; i < n; i++)
+        {
+            answer[i] = (long)x * (i + 1);
+        }
+        return answer;
+    }
+}
